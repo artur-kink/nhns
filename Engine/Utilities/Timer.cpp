0f6cb91134e8d6e1a62029a587c47ce7b080a86a
Artur Kink <malo@localhost.localdomain>
2015-01-21 15:20:15 -0500

    Added methods to Timer that do not require passing the current time as an argument. These methods use Time::getFrameTime as the current time instead.

--- Engine/Utilities/Timer.cpp
+++ Engine/Utilities/Timer.cpp
@@ -1,4 +1,5 @@
 #include "Timer.hpp"
+#include "Time.hpp"
 
 /** 
  * Create a default timer that starts at time 0 and has an interval of 1 second.
@@ -26,11 +27,30 @@ void Timer::setInterval(unsigned int i){
 }
 
 /**
+ * Reset timer to current frame time.
+ */
+void Timer::reset(){
+    startTime = Time::getInstance()->getFrameTime();
+}
+
+/**
  * Reset timer.
  * @param t Time of reset.
  */
 void Timer::reset(unsigned int t){
     startTime = t;
+}
+
+/**
+ * Check if interval time has elapsed.
+ * @return true if elapsed, false otherwise.
+ */
+bool Timer::hasElapsed(){
+    if(Time::getInstance()->getFrameTime() - startTime > interval){
+        startTime = Time::getInstance()->getFrameTime();
+        return true;
+    }
+    return false;
 }
 
 /**
@@ -59,9 +79,33 @@ bool Timer::hasElapsed(unsigned int t, unsigned int i){
 }
 
 /**
+ * Get amount of time that has elapsed since last reset.
+ */
+unsigned int Timer::getElapsedTime(){
+    return Time::getInstance()->getFrameTime() - startTime;
+}
+
+/**
  * Get the amount of time that has elapsed since last reset.
  * @param t Current time. 
  */
 unsigned int Timer::getElapsedTime(unsigned int t){
     return t - startTime;
+}
+
+/**
+ * Get the amount of time elapsed as a ratio of interval.
+ * @return ElapsedTime/Interval
+ */
+float Timer::getElapsedRatio(){
+    return (float)getElapsedTime()/(float)interval;
+}
+
+/**
+ * Get the amount of time elapsed as a ratio of interval.
+ * @param t Current time.
+ * @return ElapsedTime/Interval
+ */
+float Timer::getElapsedRatio(unsigned int t){
+    return (float)getElapsedTime(t)/(float)interval;
 }
--- Engine/Utilities/Timer.hpp
+++ Engine/Utilities/Timer.hpp
@@ -7,7 +7,9 @@
  * passed since the last reset call. Can also be used to
  * check if a time interval has passed since last reset
  * by either setting a default interval or passing desired interval.
- * All inputs of time should be from the same source.
+ * All inputs of time should be from the same source. 
+ * Method calls that do not take the current time as an argument use
+ * Time::getFrameTime() as the current time.
  */
 class Timer{
 private:
@@ -20,12 +22,19 @@ public:
     Timer();
     Timer(unsigned int t, unsigned int i);
 
-    
+    unsigned int getElapsedTime();
     unsigned int getElapsedTime(unsigned int t);
+    
+    float getElapsedRatio();
+    float getElapsedRatio(unsigned int t);
+    
+    bool hasElapsed();
     bool hasElapsed(unsigned int t);
     bool hasElapsed(unsigned int t, unsigned int i);
     
     void setInterval(unsigned int i);
+    
+    void reset();
     void reset(unsigned int t);    
 };
 
